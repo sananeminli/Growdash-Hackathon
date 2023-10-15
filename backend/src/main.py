1ba5bf9c34d5cfe68c9bfbcd664990e7ff926e09
SADIK KUZU <sadikkuzu@hotmail.com>
2023-10-15 12:23:04 +0300

    Date cleaning in ratings_chart

--- backend/src/main.py
+++ backend/src/main.py
@@ -97,14 +97,14 @@ def get_branch_detail(branch_id):
             },
         ],
         "ratings_chart": [
-            {"date": "Date:2023-10-09T17:48:35Z", "rating": 4.1},
-            {"date": "Date:2023-10-10T17:48:35Z", "rating": 4.4},
-            {"date": "Date:2023-10-11T17:48:35Z", "rating": 4.9},
-            {"date": "Date:2023-10-12T17:48:35Z", "rating": 4.8},
-            {"date": "Date:2023-10-13T17:48:35Z", "rating": 4.0},
-            {"date": "Date:2023-10-14T17:48:35Z", "rating": 3.1},
-            {"date": "Date:2023-10-15T17:48:35Z", "rating": 3.5},
-            {"date": "Date:2023-10-16T17:48:35Z", "rating": 4.1},
+            {"date": "2023-10-09T17:48:35Z", "rating": 4.1},
+            {"date": "2023-10-10T17:48:35Z", "rating": 4.4},
+            {"date": "2023-10-11T17:48:35Z", "rating": 4.9},
+            {"date": "2023-10-12T17:48:35Z", "rating": 4.8},
+            {"date": "2023-10-13T17:48:35Z", "rating": 4.1},
+            {"date": "2023-10-14T17:48:35Z", "rating": 3.1},
+            {"date": "2023-10-15T17:48:35Z", "rating": 3.5},
+            {"date": "2023-10-16T17:48:35Z", "rating": 4.1},
         ],
     }
     return JSONResponse(result)
@@ -113,13 +113,13 @@ def get_branch_detail(branch_id):
 @app.get("/branch/{branch_id}/chart")
 def get_branch_chart(branch_id):
     result = [
-        {"date": "Date:2023-10-09T17:48:35Z", "rating": 4.1},
-        {"date": "Date:2023-10-10T17:48:35Z", "rating": 4.4},
-        {"date": "Date:2023-10-11T17:48:35Z", "rating": 4.9},
-        {"date": "Date:2023-10-12T17:48:35Z", "rating": 4.8},
-        {"date": "Date:2023-10-13T17:48:35Z", "rating": 4.0},
-        {"date": "Date:2023-10-14T17:48:35Z", "rating": 3.1},
-        {"date": "Date:2023-10-15T17:48:35Z", "rating": 3.5},
-        {"date": "Date:2023-10-16T17:48:35Z", "rating": 4.1},
+        {"date": "2023-10-09T17:48:35Z", "rating": 4.1},
+        {"date": "2023-10-10T17:48:35Z", "rating": 4.4},
+        {"date": "2023-10-11T17:48:35Z", "rating": 4.9},
+        {"date": "2023-10-12T17:48:35Z", "rating": 4.8},
+        {"date": "2023-10-13T17:48:35Z", "rating": 4.0},
+        {"date": "2023-10-14T17:48:35Z", "rating": 3.1},
+        {"date": "2023-10-15T17:48:35Z", "rating": 3.5},
+        {"date": "2023-10-16T17:48:35Z", "rating": 4.1},
     ]
     return JSONResponse(result)
